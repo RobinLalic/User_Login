67b753571a4f54473a155028f1f8104fef9919db
RobinLalic <lalic.robin@gmail.com>
2023-02-22 14:20:03 +0100

    Added properties
    Added properties and their functionalities.

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,115 @@
+package com.example.demo.model;
+
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
+
+
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+    @SequenceGenerator(
+            name = "userSequence" ,
+            sequenceName = "userSequence",
+            allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "userSequence"
+    )
+    private int id;
+
+    @NotNull(message = "Please enter First Name")
+    @Column(name = "firstName")
+    private String firstName;
+
+    @NotNull(message = "Please enter Last Name")
+    @Column(name = "lastName")
+    private String lastName;
+
+    @NotNull(message = "Please enter an email address")
+    @Email(message = "Please enter a valid email address")
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @NotNull(message = "Password must not be empty")
+    @Length(min = 8, message = "Password must contain atleast 8 characters")
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "mobile", unique = true)
+    @Length(min = 10, message = "Password should contain atleast 10 characters")
+    private String mobile;
+
+    @CreationTimestamp
+    @Column(name = "createdAt", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updatedAt")
+    private LocalDateTime updatedAt;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "locked")
+    private Boolean locked = false;
+
+    @Column(name = "enabled")
+    private Boolean enabled = true;
+
+    @Override
+    public  Collection<? extends GrantedAuthority> getAuthorities(){
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(role.name());
+        return Collections.singletonList(authority);
+
+    }
+
+    @Override
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    @Override
+    public String getUsername(){ return email; }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return !locked; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return enabled; }
+
+    public com.example.demo.model.Role getRole() { return role; }
+    public void setRole(com.example.demo.model.Role role){ this.role = role; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getMobile() { return mobile; }
+    public void setMobile(String mobile) { this.mobile = mobile; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+
+}
